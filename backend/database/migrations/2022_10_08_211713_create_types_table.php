f59728baa0b272d0c8e453b314ad07870c4e6457
Diego <salazar.gonzalez.d@gmail.com>
2022-10-08 17:48:06 -0500

    Creando migraciones para la base de datos

---
+++ backend/database/migrations/2022_10_08_211713_create_types_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 45);
+            $table->string('description', 256);
+            $table->integer('guests');
+            $table->integer('beds');
+            $table->integer('bathrooms');
+            $table->float('price');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('types');
+    }
+};
---
+++ backend/database/migrations/2022_10_08_213152_create_bookings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_fullname', 100);
+            $table->string('email',45);
+            $table->float('price');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
