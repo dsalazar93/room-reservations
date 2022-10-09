704e52573cf15cb43391f5ffcf168b88daaf658d
Diego <salazar.gonzalez.d@gmail.com>
2022-10-08 19:47:45 -0500

    Creando seeders para poblar bd

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,48 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('types')->insert([
+            'name' => 'Básica',
+            'description' => 'La habitación mas sencilla',
+            'guests' => 2,
+            'beds' => 1,
+            'bathrooms' => 1,
+            'price' => 100000,
+        ]);
+        DB::table('types')->insert([
+            'name' => 'Media',
+            'description' => 'La habitación mas común',
+            'guests' => 4,
+            'beds' => 2,
+            'bathrooms' => 1,
+            'price' => 200000,
+        ]);
+        DB::table('types')->insert([
+            'name' => 'Premium',
+            'description' => 'La habitación con mas comodidades',
+            'guests' => 6,
+            'beds' => 3,
+            'bathrooms' => 3,
+            'price' => 500000,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        for ($i=0; $i < 20; $i++) {
+            $typeId = 1;
+
+            if($i > 6){
+                $typeId = 2;
+            }
+
+            if($i > 16){
+                $typeId = 3;
+            }
+
+
+            DB::table('rooms')->insert([
+                'number' => 101 + $i,
+                'description' => 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+                'type_id' => $typeId
+            ]);
+        }
     }
 }
